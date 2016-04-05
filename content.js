14d39cea17f490dab3eaec9058abb54dc759e9f5
Sash <mozhario@gmail.com>
2016-04-05 21:55:40 +0300

    final?

--- content.js
+++ content.js
@@ -1,17 +1,11 @@
-function getStyle(el, cssprop){
-	if (el.currentStyle) 
-		return el.currentStyle[cssprop]
-	else if (document.defaultView && document.defaultView.getComputedStyle) 
-		return document.defaultView.getComputedStyle(el, "")[cssprop]
-	else 
-		return el.style[cssprop]
-}
-
-//var list = document.querySelector('.im-page--dialogs')
-var dialog= document.querySelector('.im-page--history._im_page_history')
-
-var margin = getStyle(dialog, 'margin-left')
-
-var sheet = window.document.styleSheets[0]
-sheet.insertRule('.im-page--history { margin-left: 0 !important; margin-right: ' + margin + ' !important;}', sheet.cssRules.length)
-sheet.insertRule('.im-page--dialogs { float: right !important; }', sheet.cssRules.length)
+var margin = '317px'
+var customStyles = document.createElement('style'); 
+customStyles.innerHTML = 
+    '.im-page--history { '+ 
+    	'margin-left: 0 !important; ' +
+    	'margin-right: ' + margin + ' !important;' +
+    '}' +
+    '.im-page--dialogs { ' +
+    	'float: right !important; ' +
+    '}';
+document.documentElement.insertBefore(customStyles, null);
